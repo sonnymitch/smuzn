545f821db54e70ff4167613e73fa0220adb01787
David Sonny Mihaescu <davidsonnymihaescu@gmail.com>
2025-06-02 21:35:02 +0200

    Update index.html

---
+++ .php/preloads.php
@@ -0,0 +1,4 @@
+<!-- Preload scripts -->
+<link rel="preload" href="/script/bootstrap.bundle.min.js" as="script">
+<link rel="preload" href="/script/jquery-3.7.1.slim.min.js" as="script">
+<link rel="preload" href="/script/jquery-scripts.js" as="script">
